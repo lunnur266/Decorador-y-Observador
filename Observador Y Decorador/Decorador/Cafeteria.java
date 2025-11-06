32f82d0cc77bd20fe4cdcd216628877f32548ade
lunnur266 <167033869+lunnur266@users.noreply.github.com>
2025-11-06 07:42:26 -0500

    Update beverage description to include cacao option
    Added option for cacao in the description of the beverage.

--- Observador Y Decorador/Decorador/Cafeteria.java
+++ Observador Y Decorador/Decorador/Cafeteria.java
@@ -2,9 +2,10 @@ public class Cafeteria {
     public static void main(String[] args) {
         Bebida bebida = new Cafe();              // Café base
         bebida = new Leche(bebida);              // Se le agrega leche o se cambia por la de leche de coco
-        bebida = new Canela(bebida);             // Se le agrega canela
+        bebida = new Canela(bebida);             // Se le agrega canela o cacao en polvo
 
         System.out.println(bebida.getDescripcion() + " cuesta $" + bebida.getCosto());
     }
 
 }
+
