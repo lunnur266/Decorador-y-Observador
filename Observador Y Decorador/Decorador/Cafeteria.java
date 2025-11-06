58e8c9595d68904e287304ed3178607a258bfe13
lunnur266 <167033869+lunnur266@users.noreply.github.com>
2025-11-06 07:41:19 -0500

    Update comment for adding milk option in Cafeteria

--- Observador Y Decorador/Decorador/Cafeteria.java
+++ Observador Y Decorador/Decorador/Cafeteria.java
@@ -1,9 +1,10 @@
 public class Cafeteria {
     public static void main(String[] args) {
         Bebida bebida = new Cafe();              // Café base
-        bebida = new Leche(bebida);              // Se le agrega leche
+        bebida = new Leche(bebida);              // Se le agrega leche o se cambia por la de leche de coco
         bebida = new Canela(bebida);             // Se le agrega canela
 
         System.out.println(bebida.getDescripcion() + " cuesta $" + bebida.getCosto());
     }
+
 }
